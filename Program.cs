3064a0cb88d74b6ca0940c6f04ba5d8e06b697c2
FL <felip3.fl@gmail.com>
2024-06-17 22:29:00 -0300

    Method to Add Line

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿
-var teste = File.ReadAllText("gameList.csv");
-Console.WriteLine("Hello, World!");
+﻿var address = "E:\\ReadWriteTextFile\\gameList.csv";
+var teste = File.ReadAllText(address);
+Console.WriteLine(teste);
+
+Console.WriteLine("\n ------------------------------- \n");
+
+lineChanger("TESTE", address, 2);
+Console.WriteLine(teste);
+
+void lineChanger(string newText, string fileName, int line_to_edit)
+{
+    string[] arrLine = File.ReadAllLines(fileName);
+    arrLine[line_to_edit - 1] = newText;
+    File.WriteAllLines(fileName, arrLine);
+}
